cdea00e8e90c6b17b4455616d71f29d1e8ae147a
bryanb213 <blancasbryan@gmail.com>
2020-06-12 18:43:24 -0500

    added mapper

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using NetApi.Data;
+using NetApi.Dtos;
 using NetApi.Models;
 
 namespace NetApi.Controllers
@@ -10,24 +12,31 @@ namespace NetApi.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepo _repo;
-        public CommandsController(ICommanderRepo context)
+        private readonly IMapper _map;
+        public CommandsController(ICommanderRepo context, IMapper mapper)
         {
             _repo = context;
+            _map = mapper;
+
         }
 
         [HttpGet]
-        public ActionResult <IEnumerable<Command>> GetCommands()
+        public ActionResult <IEnumerable<CommandDto>> GetCommands()
         {
             var commands = _repo.GetCommands();
-            return Ok(commands);
+            return Ok(_map.Map<IEnumerable<CommandDto>>(commands));
         }
 
         [HttpGet("{id}")]
-        public ActionResult <Command> GetCommandById(int id)
+        public ActionResult <CommandDto> GetCommandById(int id)
         {
             var command =_repo.GetCommandById(id);
+            if(command != null)
+            {
+                return Ok(_map.Map<CommandDto>(command));
+            }
 
-            return Ok(command);
+            return NotFound();
         }
     }
 }
